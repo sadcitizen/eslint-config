54d0626bda09f5ed5f91a74bbd774c1ac3115095
sadcitizen <sadcitizen@yandex.ru>
2018-05-26 16:17:00 +0500

    empty import and jest configs

---
+++ import.js
@@ -0,0 +1,12 @@
+/**
+ * https://github.com/benmosher/eslint-plugin-import
+ */
+module.exports = {
+    parser: 'babel-eslint',
+    plugins: [
+        'import'
+    ],
+    rules: {
+
+    }
+};
---
+++ jest.js
@@ -0,0 +1,15 @@
+/**
+ * https://github.com/jest-community/eslint-plugin-jest
+ */
+module.exports = {
+    parser: 'babel-eslint',
+    env: {
+        jest: true
+    },
+    plugins: [
+        'jest'
+    ],
+    rules: {
+
+    }
+};
