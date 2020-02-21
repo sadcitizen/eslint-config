3cfafa3554b3e40e893d3f427b7232c8a1871077
sadcitizen <sadcitizen@yandex.ru>
2020-02-21 23:21:44 +0500

    add react hooks plugin

---
+++ react-hooks.js
@@ -0,0 +1,12 @@
+/**
+ * https://github.com/facebook/react/tree/master/packages/eslint-plugin-react-hooks
+ */
+module.exports = {
+    plugins: [
+        'react-hooks'
+    ],
+    rules: {
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'warn'
+    }
+};
