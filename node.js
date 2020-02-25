3503d9f5af8be72bf69066c7ad61d36781b8873a
sadcitizen <sadcitizen@yandex.ru>
2020-02-25 08:45:01 +0500

    node plugin

---
+++ node.js
@@ -0,0 +1,14 @@
+/**
+ * https://github.com/mysticatea/eslint-plugin-node
+ */
+module.exports = {
+    plugins: [
+        'node'
+    ],
+    extends: [
+        'plugin:node/recommended'
+    ],
+    rules: {
+
+    }
+};
